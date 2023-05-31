a78006a0eac51c2c1745acb23767d635eaff344c
Quang Nguyen <133824929+Zitherlight@users.noreply.github.com>
2023-05-30 20:26:07 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-        int my_variable = 100
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
